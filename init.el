189c073bb5afd826fce292f9e62f83336541b9cd
Peter Borocz <680893+PBorocz@users.noreply.github.com>
2022-11-23 14:28:02 -0800

    Add abbrev mode using common typos from Wikipedia.

--- init.el
+++ init.el
@@ -46,8 +46,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(use-package smart-jump gruvbox-theme doom-modeline diminish)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
