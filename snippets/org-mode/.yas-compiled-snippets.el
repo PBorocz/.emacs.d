25b96dff02225f53d8478f6d2a8c10309a31ed2e
Peter Borocz <680893+PBorocz@users.noreply.github.com>
2022-11-27 15:17:48 -0800

    Really missed snippets.

---
+++ snippets/org-mode/.yas-compiled-snippets.el
@@ -0,0 +1,9 @@
+;;; Compiled snippets and support files for `org-mode'
+;;; Snippet definitions:
+;;;
+(yas-define-snippets 'org-mode
+                     '(("block_" "#+BEGIN_SRC $1\n  $0\n#+END_SRC\n" "#+begin_...#+end_" nil nil nil "/Users/peter/.emacs.d/snippets/org-mode/block" nil nil)
+                       ("src_" "#+BEGIN_SRC $1\n  $0\n#+END_SRC\n" "#+begin_...#+end_" nil nil nil "/Users/peter/.emacs.d/snippets/org-mode/begin" nil nil)))
+
+
+;;; Do not edit! File generated at Tue Nov  2 16:35:21 2021
