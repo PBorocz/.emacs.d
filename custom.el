75584261bd0c301f8e5dd2466c91ce83c4a858e7
Peter Borocz <680893+PBorocz@users.noreply.github.com>
2022-12-25 16:56:25 -0800

    Vertico instead of ido, Cleanup init.el.

---
+++ custom.el
@@ -0,0 +1,14 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(csv-mode mini-frame org-cliplink zygospore web-mode vterm vlf use-package smart-jump rg rainbow-mode rainbow-delimiters pyvenv-auto pdf-tools org-modern org-bullets markdown-mode magit hungry-delete gruvbox-theme graphviz-dot-mode git-timemachine git-gutter esup elm-mode eglot editorconfig doom-modeline diredfl dired-narrow dimmer diminish counsel company beacon apples-mode anzu all-the-icons-ivy-rich)))
+
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
--- init.el
+++ init.el
@@ -3,22 +3,23 @@
   (message
    "Emacs loaded in %s with %d garbage collections."
    (format "%.2f seconds" (float-time (time-subtract after-init-time before-init-time)))
-   gcs-done))
+   gcs-done)
+  )
 (add-hook 'emacs-startup-hook #'pb/_display-startup-time)
 
 ;; Start "standard"
 (let ((gc-cons-threshold most-positive-fixnum))
 
   ;; Do this right up front to allow us to use VC-controlled
-  ;; configuration files later on in the startup process (by the time
-  ;; we hit the same setting in the tangled config file (config.el),
-  ;; it's too late.
+  ;; configuration files later on in the startup process [as by the
+  ;; time we hit the same setting in the tangled config file
+  ;; (config.el), it's too late].
   (setq vc-follow-symlinks t)
 
-  ;; Set repositories
+  ;; Set up repositories
   (require 'package)
-  (add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
   (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+  (add-to-list 'package-archives '("org"   . "https://orgmode.org/elpa/"))
   (package-initialize)
 
   (unless (package-installed-p 'use-package)
@@ -27,32 +28,21 @@
 
   (eval-and-compile
     (setq use-package-always-ensure    t
-          use-package-always-defer     t
           use-package-expand-minimally t))
 
-  (setq vc-follow-symlinks "t")
+  ;; Set the location of a separate "custom" file (ie. custom-set-variables and custom-set-faces).
+  ;; (we don't want it stored below ;-()
+  (use-package cus-edit
+    :custom
+    (custom-file (expand-file-name "custom.el" user-emacs-directory))
+    :config
+    (load custom-file 'noerror))
 
-  ;; Tangle and get our configuration from org file:
-  ;; (add-to-list 'load-path "~/.emacs.d/lisp/org-menu")
-  ;; (org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
+  ;; Tangle and get our configuration from our config.org file:
   (require 'org)
   (org-babel-load-file (expand-file-name (concat user-emacs-directory "config.org")))
 
-  ;; Cleanup
+  ;; Cleanup and we're done!
   (garbage-collect)
+
   )
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(csv-mode mini-frame org-cliplink zygospore web-mode vterm vlf use-package smart-jump rg rainbow-mode rainbow-delimiters pyvenv-auto pdf-tools org-modern org-bullets markdown-mode magit hungry-delete gruvbox-theme graphviz-dot-mode git-timemachine git-gutter esup elm-mode eglot editorconfig doom-modeline diredfl dired-narrow dimmer diminish counsel company beacon apples-mode anzu all-the-icons-ivy-rich)))
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
