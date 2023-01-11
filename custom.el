c3102e69d9eeeaa6c88fa642e4310d18c0f1b744
Peter Borocz <680893+PBorocz@users.noreply.github.com>
2023-01-11 10:19:36 -0800

    With emacs-27+, we don't need extended selection, simply use fido now!

--- custom.el
+++ custom.el
@@ -13,4 +13,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-level-1 ((t (:inherit default :extend nil :height 1.1))))
+ '(org-level-2 ((t (:inherit default :extend nil :height 1.1))))
+ '(org-level-3 ((t (:inherit default :extend nil :height 1.0))))
+ '(org-level-4 ((t (:inherit default :extend nil :height 1.0))))
  )
