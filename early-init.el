1919799994fc16967d618840292e09595d51606c
Peter Borocz <680893+PBorocz@users.noreply.github.com>
2023-01-16 16:20:33 -0800

    Medium refactoring to make startup nicer by using early-init.el.
    And, going back to SourceCodePro do to missing stuff in RedHat?

---
+++ early-init.el
@@ -0,0 +1,64 @@
+;; -----------------------------------------------------------------------------
+;; Nicer handling of setting GC threshold for initialisation only
+;; (reset afterwards for normal operation)
+;;
+;; Both of the following from:
+;; https://www.reddit.com/r/emacs/comments/3kqt6e/2_easy_little_known_steps_to_speed_up_emacs_start/
+;; -----------------------------------------------------------------------------
+;; Set garbage collection threshold
+(setq gc-cons-threshold-original gc-cons-threshold)
+(setq gc-cons-threshold most-positive-fixnum)
+
+(setq file-name-handler-alist-original file-name-handler-alist)
+(setq file-name-handler-alist nil)
+
+;; Set deferred timer to reset them
+(run-with-idle-timer 2 nil
+ (lambda ()
+   (setq gc-cons-threshold gc-cons-threshold-original)
+   (setq file-name-handler-alist file-name-handler-alist-original)
+   (makunbound 'gc-cons-threshold-original)
+   (makunbound 'file-name-handler-alist-original)
+   (message "(...restored original values of 'gc-cons-threshold' and 'file-name-handler-alist'.)")))
+;; -----------------------------------------------------------------------------
+
+;; Native compilation settings
+(when (fboundp 'native-comp-available-p)
+  ;; Set the right directory to store the native compilation cache
+  (let ((path (expand-file-name "eln-cache/" user-emacs-directory)))
+    (setq native-comp-eln-load-path (list path)
+          native-compile-target-directory path))
+  (setq native-comp-async-report-warnings-errors nil ;; Silence compiler warnings as they can be pretty disruptive
+        inhibit-automatic-native-compilation     t)  ;; Make native compilation happens asynchronously
+  )
+
+;; Theme setting
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+(load-theme 'monokai t)
+
+;; Reduce the font-size differences of org-mode headlines.
+(setq monokai-height-plus-4 1.2
+      monokai-height-plus-3 1.1
+      monokai-height-plus-2 1.0)
+
+;; Prefer loading newest compiled .el file
+(setq load-prefer-newer         noninteractive
+      package-enable-at-startup t
+      inhibit-startup-message   t)
+
+(set-window-scroll-bars (minibuffer-window) nil nil)
+(set-default-coding-systems 'utf-8) ;; Set default coding system (especially for Windows)
+
+(blink-cursor-mode     1)
+(column-number-mode    t)
+(global-font-lock-mode 1)
+(menu-bar-mode         -1)
+(scroll-bar-mode       -1)
+(tool-bar-mode         -1)
+(tooltip-mode          -1)
+
+(put 'downcase-region 'disabled nil)
+(put 'upcase-region   'disabled nil)
+
+;; Make the initial buffer load faster by setting its mode to fundamental-mode
+(setq initial-major-mode 'fundamental-mode)
--- init.el
+++ init.el
@@ -1,4 +1,48 @@
-;; Startup time
+
+;; Do this right up front to allow us to use VC-controlled
+;; configuration files later on in the startup process [as by the
+;; time we hit the same setting in the tangled config file
+;; (config.el), it's too late].
+(setq vc-follow-symlinks t)
+
+;; Emacs by default caps the number of bytes read from a subprocess
+;; in a single chunk to 4KB. However, modern machines can take on a
+;; lot more. Set it to 1MB which is equal to the limit defined
+;; in /proc/sys/fs/pipe-max-size/.
+;; Ref: https://grtcdr.tn/dotfiles/emacs/emacs.html#orgdb7d3a6
+(setq read-process-output-max (* 1024 1024))
+
+  ;; Set up repositories
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+(add-to-list 'package-archives '("org"   . "https://orgmode.org/elpa/"))
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+(eval-and-compile
+  (setq use-package-always-ensure    t
+        use-package-expand-minimally t))
+
+;; Set the location of a separate "custom" file (ie. custom-set-variables and custom-set-faces).
+;; (we don't want it stored below in this file. ;-()
+(use-package cus-edit
+  :custom
+  (custom-file (expand-file-name "custom.el" user-emacs-directory))
+  :config
+  (load custom-file 'noerror))
+
+;; If necessary for debugging config issues..
+;; (setq debug-on-error 1)
+;; (setq debug-on-quit t)
+
+;; Get the rest of our configuration from our tangled config.org file:
+(require 'org)
+(org-babel-load-file (expand-file-name (concat user-emacs-directory "config.org")))
+
+;; Display startup time
 (defun pb/_display-startup-time ()
   (message
    "Emacs loaded in %s with %d garbage collections."
@@ -6,54 +50,3 @@
    gcs-done)
   )
 (add-hook 'emacs-startup-hook #'pb/_display-startup-time)
-
-;; Start "standard"
-(let ((gc-cons-threshold most-positive-fixnum))
-
-  ;; Do this right up front to allow us to use VC-controlled
-  ;; configuration files later on in the startup process [as by the
-  ;; time we hit the same setting in the tangled config file
-  ;; (config.el), it's too late].
-  (setq vc-follow-symlinks t)
-
-  ;; Emacs by default caps the number of bytes read from a subprocess
-  ;; in a single chunk to 4KB. However, modern machines can take on a
-  ;; lot more. I'll set it to 1MB which is equal to the limit defined
-  ;; in /proc/sys/fs/pipe-max-size/.
-  ;; ht: https://grtcdr.tn/dotfiles/emacs/emacs.html#orgdb7d3a6
-  (setq read-process-output-max (* 1024 1024))
-
-  ;; Set up repositories
-  (require 'package)
-  (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
-  (add-to-list 'package-archives '("org"   . "https://orgmode.org/elpa/"))
-  (package-initialize)
-
-  (unless (package-installed-p 'use-package)
-    (package-refresh-contents)
-    (package-install 'use-package))
-
-  (eval-and-compile
-    (setq use-package-always-ensure    t
-          use-package-expand-minimally t))
-
-  ;; Set the location of a separate "custom" file (ie. custom-set-variables and custom-set-faces).
-  ;; (we don't want it stored below ;-()
-  (use-package cus-edit
-    :custom
-    (custom-file (expand-file-name "custom.el" user-emacs-directory))
-    :config
-    (load custom-file 'noerror))
-
-  ;; If necessary for debugging config issues..
-  ;; (setq debug-on-error 1)
-  ;; (setq debug-on-quit t)
-
-  ;; Tangle and get our configuration from our config.org file:
-  (require 'org)
-  (org-babel-load-file (expand-file-name (concat user-emacs-directory "config.org")))
-
-  ;; Cleanup and we're done!
-  (garbage-collect)
-
-  )
