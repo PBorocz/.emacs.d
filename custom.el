d82f7b728712c962a5c596ef161a61381bfa5bb4
Peter Borocz <680893+PBorocz@users.noreply.github.com>
2023-03-21 10:28:32 -0700

    Cleanup of some packages we don't use (or get in our way).

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(custom-safe-themes
    '("37c8c2817010e59734fe1f9302a7e6a2b5e8cc648cf6a6cc8b85f3bf17fececf" "5fdc0f5fea841aff2ef6a75e3af0ce4b84389f42e57a93edc3320ac15337dc10" "53585ce64a33d02c31284cd7c2a624f379d232b27c4c56c6d822eff5d3ba7625" "72ed8b6bffe0bfa8d097810649fd57d2b598deef47c992920aef8b5d9599eefe" default))
  '(package-selected-packages
-   '(helpful toml-mode pbcopy flycheck orderless ido-vertical-mode edit-indirect yaml-mode ibuffer-git csv-mode mini-frame org-cliplink zygospore web-mode vterm vlf use-package smart-jump rg rainbow-mode rainbow-delimiters pyvenv-auto pdf-tools org-modern org-bullets markdown-mode magit hungry-delete gruvbox-theme graphviz-dot-mode git-timemachine git-gutter esup elm-mode eglot editorconfig doom-modeline diredfl dired-narrow dimmer diminish counsel company beacon apples-mode anzu all-the-icons-ivy-rich)))
+   '(smart-hungry-delete org-web-tools helpful toml-mode pbcopy flycheck orderless ido-vertical-mode edit-indirect yaml-mode ibuffer-git csv-mode mini-frame org-cliplink zygospore web-mode vterm vlf use-package smart-jump rg rainbow-mode rainbow-delimiters pyvenv-auto pdf-tools org-modern org-bullets markdown-mode magit hungry-delete gruvbox-theme graphviz-dot-mode git-timemachine git-gutter esup elm-mode eglot editorconfig doom-modeline diredfl dired-narrow dimmer diminish counsel company beacon apples-mode anzu all-the-icons-ivy-rich)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
