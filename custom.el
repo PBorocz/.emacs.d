14774eb984deff6bfadbcc97f3d03911b2dd69a9
Peter Borocz <680893+PBorocz@users.noreply.github.com>
2023-06-11 13:53:17 -0700

    Make title bar transparent, completely remove C-x 2/3 customisation.

--- custom.el
+++ custom.el
@@ -4,10 +4,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(hide-mode-line org-bulletproof hl-line+ jinx org-superstar org-appear zygospore yasnippet yaml-mode yaml web-mode vterm vlf vertico use-package toml-mode toc-org smart-jump smart-hungry-delete rg rainbow-mode rainbow-delimiters pyvenv-auto pdf-tools pbcopy org-web-tools org-modern org-cliplink org-bullets orderless mini-frame markdown-mode magit just-mode ido-vertical-mode ibuffer-git hungry-delete helpful gruvbox-theme graphviz-dot-mode git-timemachine git-gutter flycheck fish-mode esup elm-mode eglot editorconfig edit-indirect doom-modeline diredfl dired-narrow dimmer diminish csv-mode counsel company beacon apples-mode anzu all-the-icons-ivy-rich)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
+   '(hide-mode-line org-bulletproof hl-line+ jinx org-superstar org-appear zygospore yasnippet yaml-mode yaml web-mode vterm vlf vertico use-package toml-mode toc-org smart-jump smart-hungry-delete rg rainbow-mode rainbow-delimiters pyvenv-auto pdf-tools pbcopy org-web-tools org-modern org-cliplink org-bullets orderless mini-frame markdown-mode magit just-mode ido-vertical-mode ibuffer-git hungry-delete helpful gruvbox-theme graphviz-dot-mode git-timemachine git-gutter flycheck fish-mode esup elm-mode eglot editorconfig edit-indirect doom-modeline diredfl dired-narrow dimmer diminish csv-mode counsel company beacon apples-mode anzu all-the-icons-ivy-rich))
  )
