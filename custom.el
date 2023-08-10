a4e3737fa847bbe5378425458bfa2922e07a34c3
Peter Borocz <680893+PBorocz@users.noreply.github.com>
2023-08-10 11:58:34 -0700

    Turn off git gutter in org-mode; Add grammarly (but'll wait for tree-sitter).

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(annotate ct format-all volatile-highlights olivetti emacs-gc-stats gptel hide-mode-line org-bulletproof hl-line+ jinx org-superstar org-appear zygospore yasnippet yaml-mode yaml web-mode vterm vlf vertico use-package toml-mode toc-org smart-jump smart-hungry-delete rg rainbow-mode rainbow-delimiters pyvenv-auto pdf-tools pbcopy org-web-tools org-modern org-cliplink org-bullets orderless mini-frame markdown-mode magit just-mode ido-vertical-mode ibuffer-git hungry-delete helpful gruvbox-theme graphviz-dot-mode git-timemachine git-gutter flycheck fish-mode esup elm-mode eglot editorconfig edit-indirect doom-modeline diredfl dired-narrow dimmer diminish csv-mode counsel company beacon apples-mode anzu all-the-icons-ivy-rich))
+   '(eglot-grammarly annotate ct format-all volatile-highlights olivetti emacs-gc-stats gptel hide-mode-line org-bulletproof hl-line+ jinx org-superstar org-appear zygospore yasnippet yaml-mode yaml web-mode vterm vlf vertico use-package toml-mode toc-org smart-jump smart-hungry-delete rg rainbow-mode rainbow-delimiters pyvenv-auto pdf-tools pbcopy org-web-tools org-modern org-cliplink org-bullets orderless mini-frame markdown-mode magit just-mode ido-vertical-mode ibuffer-git hungry-delete helpful gruvbox-theme graphviz-dot-mode git-timemachine git-gutter flycheck fish-mode esup elm-mode eglot editorconfig edit-indirect doom-modeline diredfl dired-narrow dimmer diminish csv-mode counsel company beacon apples-mode anzu all-the-icons-ivy-rich))
  '(safe-local-variable-values '((olivetti-body-width . 0.66)))
  '(warning-suppress-types '((emacs))))
 (custom-set-faces
