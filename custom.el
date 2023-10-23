be6bc6283523c5ca60a50c5305a9d76ae3e2b384
Peter Borocz <680893+PBorocz@users.noreply.github.com>
2023-10-23 15:18:09 -0700

    Change modeline (to spaceline from doom) and rg (from rg to deadgrep).

--- custom.el
+++ custom.el
@@ -4,10 +4,10 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("01f14217a46c1dbad5a23eeb191937f89babc8ee5cfd1038c3e459b91fca28f6" default))
+   '("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "01f14217a46c1dbad5a23eeb191937f89babc8ee5cfd1038c3e459b91fca28f6" default))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(package-selected-packages
-   '(fullframe origami autothemer async f hl-todo pcre2el yaml-pro treesit-auto eglot annotate ct format-all volatile-highlights olivetti emacs-gc-stats gptel hide-mode-line org-bulletproof hl-line+ jinx org-superstar org-appear zygospore yasnippet yaml-mode yaml web-mode vterm vlf vertico use-package toml-mode toc-org smart-jump smart-hungry-delete rg rainbow-mode rainbow-delimiters pyvenv-auto pdf-tools pbcopy org-web-tools org-modern org-cliplink org-bullets orderless mini-frame markdown-mode magit just-mode ido-vertical-mode ibuffer-git hungry-delete helpful gruvbox-theme graphviz-dot-mode git-timemachine git-gutter flycheck fish-mode esup elm-mode editorconfig edit-indirect doom-modeline diredfl dired-narrow dimmer diminish csv-mode counsel company beacon apples-mode anzu all-the-icons-ivy-rich))
+   '(deadgrep spaceline smart-mode-line fullframe origami autothemer async f hl-todo pcre2el yaml-pro treesit-auto eglot annotate ct format-all volatile-highlights olivetti emacs-gc-stats gptel hide-mode-line org-bulletproof hl-line+ jinx org-superstar org-appear zygospore yasnippet yaml-mode yaml web-mode vterm vlf vertico use-package toml-mode toc-org smart-jump smart-hungry-delete rg rainbow-mode rainbow-delimiters pyvenv-auto pdf-tools pbcopy org-web-tools org-modern org-cliplink org-bullets orderless mini-frame markdown-mode magit just-mode ido-vertical-mode ibuffer-git hungry-delete helpful gruvbox-theme graphviz-dot-mode git-timemachine git-gutter flycheck fish-mode esup elm-mode editorconfig edit-indirect doom-modeline diredfl dired-narrow dimmer diminish csv-mode counsel company beacon apples-mode anzu all-the-icons-ivy-rich))
  '(safe-local-variable-values '((olivetti-body-width . 0.66)))
  '(warning-suppress-types '((emacs))))
 
