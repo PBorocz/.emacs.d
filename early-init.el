b41a32e209217d97431c9764d055c72451a6f4ab
Peter Borocz <680893+PBorocz@users.noreply.github.com>
2023-12-12 10:12:23 -0800

    Add bbatsov's super-save. Finally nail debug-on-quit.

--- early-init.el
+++ early-init.el
@@ -1,3 +1,9 @@
+
+;; As of 2023-12-06 (and almost daily lockups, don't worry about startup speed and focus on stability)
+;; (this has only been an issue with v29+, never had a problem with 28!)
+;; https://lists.gnu.org/archive/html/help-gnu-emacs/2007-06/msg00243.html
+(setq gc-cons-percentage 0.6)
+
 ;; -----------------------------------------------------------------------------
 ;; Nicer handling of setting GC threshold for initialisation only
 ;; (reset afterwards for normal operation)
@@ -5,22 +11,24 @@
 ;; Both of the following from:
 ;; https://www.reddit.com/r/emacs/comments/3kqt6e/2_easy_little_known_steps_to_speed_up_emacs_start/
 ;; -----------------------------------------------------------------------------
-;; Set garbage collection threshold
-(setq gc-cons-threshold-original gc-cons-threshold)
-(setq gc-cons-threshold most-positive-fixnum)
+;; Save the current threshold and set garbage collection threshold to large num..
+;; (setq gc-cons-threshold-original gc-cons-threshold)
+;; (setq gc-cons-threshold most-positive-fixnum)
 
-(setq file-name-handler-alist-original file-name-handler-alist)
-(setq file-name-handler-alist nil)
+;; ??
+;; (setq file-name-handler-alist-original file-name-handler-alist)
+;; (setq file-name-handler-alist nil)
 
-;; Set deferred timer to reset them
-(run-with-idle-timer 2 nil
- (lambda ()
-   (setq gc-cons-threshold gc-cons-threshold-original)
-   (setq file-name-handler-alist file-name-handler-alist-original)
-   (makunbound 'gc-cons-threshold-original)
-   (makunbound 'file-name-handler-alist-original)
-   )
- )
+;; ;; Set deferred timer to reset them both back to original values..
+;; (run-with-idle-timer 2 nil
+;;  (lambda ()
+;;    (setq gc-cons-threshold gc-cons-threshold-original)
+;;    (setq file-name-handler-alist file-name-handler-alist-original)
+;;
+;;    (makunbound 'gc-cons-threshold-original)
+;;    (makunbound 'file-name-handler-alist-original)
+;;    )
+;;  )
 ;; -----------------------------------------------------------------------------
 
 ;; Native compilation settings
@@ -43,9 +51,9 @@
       monokai-height-plus-2 1.0)
 
 ;; Prefer loading newest compiled .el file and turn off
-(setq load-prefer-newer noninteractive
+(setq load-prefer-newer         noninteractive
       package-enable-at-startup t
-      inhibit-startup-message t)
+      inhibit-startup-message   t)
 
 (set-window-scroll-bars (minibuffer-window) nil nil)
 (set-default-coding-systems 'utf-8)
