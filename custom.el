d486b050815314e9c57c974238102b3cc6fc53fb
Peter Borocz <680893+PBorocz@users.noreply.github.com>
2023-12-15 11:06:33 -0800

    Try out Prot's spacious-padding-mode.

--- custom.el
+++ custom.el
@@ -7,7 +7,7 @@
    '("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "01f14217a46c1dbad5a23eeb191937f89babc8ee5cfd1038c3e459b91fca28f6" default))
  '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
  '(package-selected-packages
-   '(super-save move-text marginalia spaceline smart-mode-line fullframe origami autothemer async f hl-todo pcre2el yaml-pro treesit-auto eglot annotate ct format-all volatile-highlights olivetti emacs-gc-stats gptel hide-mode-line org-bulletproof hl-line+ jinx org-superstar org-appear zygospore yasnippet yaml-mode yaml web-mode vterm vlf vertico use-package toml-mode toc-org smart-jump smart-hungry-delete rg rainbow-mode rainbow-delimiters pyvenv-auto pdf-tools pbcopy org-web-tools org-modern org-cliplink org-bullets orderless mini-frame markdown-mode magit just-mode ido-vertical-mode ibuffer-git hungry-delete helpful gruvbox-theme graphviz-dot-mode git-timemachine git-gutter flycheck fish-mode esup elm-mode editorconfig edit-indirect doom-modeline diredfl dired-narrow dimmer diminish csv-mode counsel company beacon apples-mode anzu all-the-icons-ivy-rich))
+   '(spacious-padding super-save move-text marginalia spaceline smart-mode-line fullframe origami autothemer async f hl-todo pcre2el yaml-pro treesit-auto eglot annotate ct format-all volatile-highlights olivetti emacs-gc-stats gptel hide-mode-line org-bulletproof hl-line+ jinx org-superstar org-appear zygospore yasnippet yaml-mode yaml web-mode vterm vlf vertico use-package toml-mode toc-org smart-jump smart-hungry-delete rg rainbow-mode rainbow-delimiters pyvenv-auto pdf-tools pbcopy org-web-tools org-modern org-cliplink org-bullets orderless mini-frame markdown-mode magit just-mode ido-vertical-mode ibuffer-git hungry-delete helpful gruvbox-theme graphviz-dot-mode git-timemachine git-gutter flycheck fish-mode esup elm-mode editorconfig edit-indirect doom-modeline diredfl dired-narrow dimmer diminish csv-mode counsel company beacon apples-mode anzu all-the-icons-ivy-rich))
  '(safe-local-variable-values '((olivetti-body-width . 0.66)))
  '(warning-suppress-types '((emacs))))
 
@@ -16,6 +16,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(fringe ((t :background "#272822")))
+ '(header-line ((t :box (:line-width 4 :color "#49483E" :style nil))))
+ '(header-line-highlight ((t :box (:color "#F8F8F2"))))
+ '(line-number ((t :background "#272822")))
  '(mode-line ((t (:family "SF Mono" :height 0.8))))
  '(mode-line-active ((t (:family "SauceCodePro Nerd Font Mono" :height 1.1))))
- '(mode-line-inactive ((t (:family "SF Mono" :height 0.9)))))
+ '(mode-line-highlight ((t :box (:color "#F8F8F2"))))
+ '(mode-line-inactive ((t (:family "SF Mono" :height 0.9))))
+ '(tab-bar-tab ((t :box (:line-width 4 :color "grey85" :style nil))))
+ '(tab-bar-tab-inactive ((t :box (:line-width 4 :color "grey75" :style nil))))
+ '(window-divider ((t :background "#272822" :foreground "#272822")))
+ '(window-divider-first-pixel ((t :background "#272822" :foreground "#272822")))
+ '(window-divider-last-pixel ((t :background "#272822" :foreground "#272822"))))
