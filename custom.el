611d0f52bdf6a785a926ac84161b524ad6e10ddc
Peter Borocz <680893+PBorocz@users.noreply.github.com>
2024-03-08 08:32:16 -0800

    TIL: Commenting an org entry keeps it from being tangled (!)

--- custom.el
+++ custom.el
@@ -3,13 +3,20 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-  '(package-selected-packages
-   '(mmm-mode unfill zoom mood-line super-save move-text smart-mode-line fullframe autothemer async f hl-todo pcre2el yaml-pro treesit-auto eglot annotate ct format-all volatile-highlights olivetti emacs-gc-stats gptel hide-mode-line org-bulletproof hl-line+ org-superstar org-appear zygospore yasnippet yaml-mode yaml web-mode vterm vlf vertico use-package toml-mode toc-org smart-jump smart-hungry-delete rg rainbow-mode rainbow-delimiters pyvenv-auto pdf-tools pbcopy org-web-tools org-modern org-cliplink org-bullets orderless mini-frame markdown-mode magit just-mode ibuffer-git hungry-delete helpful gruvbox-theme graphviz-dot-mode git-timemachine git-gutter flycheck edit-indirect diredfl dired-narrow dimmer diminish csv-mode counsel company apples-mode anzu all-the-icons-ivy-rich))
-  )
+ '(magit-todos-insert-after '(bottom) nil nil "Changed by setter of obsolete option `magit-todos-insert-at'")
+ '(package-selected-packages
+   '(ws-butler nerd-icons-ibuffer mmm-mode unfill zoom mood-line super-save move-text smart-mode-line fullframe autothemer async f hl-todo pcre2el yaml-pro treesit-auto eglot annotate ct format-all volatile-highlights olivetti emacs-gc-stats gptel hide-mode-line org-bulletproof hl-line+ org-superstar org-appear zygospore yasnippet yaml-mode yaml web-mode vterm vlf vertico use-package toml-mode toc-org smart-jump smart-hungry-delete rg rainbow-mode rainbow-delimiters pyvenv-auto pdf-tools pbcopy org-web-tools org-modern org-cliplink org-bullets orderless mini-frame markdown-mode magit just-mode ibuffer-git hungry-delete helpful gruvbox-theme graphviz-dot-mode git-timemachine git-gutter flycheck edit-indirect diredfl dired-narrow dimmer diminish csv-mode counsel company apples-mode anzu all-the-icons-ivy-rich)))
 
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
-) 
+ '(font-lock-comment-face ((t (:foreground "#666666" :height 0.9))))
+ '(font-lock-doc-face ((t (:foreground "#666666" :height 0.9))))
+ '(mode-line-active ((t (:family "SauceCodePro Nerd Font Mono" :height 1.0))))
+ '(mode-line-inactive ((t (:family "SauceCodePro Nerd Font Mono" :height 0.7))))
+ '(org-document-title ((t (:height 1.5 :underline nil))))
+ '(org-level-1 ((t (:height 1.15))))
+ '(org-level-2 ((t (:height 1.1))))
+ '(org-level-3 ((t (:height 1.05)))))
