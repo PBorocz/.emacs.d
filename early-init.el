85ad0addc702c1b21d8c808e8bac6df97226d374
Peter Borocz <680893+PBorocz@users.noreply.github.com>
2024-05-12 16:18:57 -0700

    Org restructuring. Add bufler instead of ibuffer.

--- early-init.el
+++ early-init.el
@@ -1,17 +1,22 @@
 ;; 2024-01-23: https://zenodo.org/records/10518083 seems to indicate this would help.
 ;;             (specifically, by increasing this we trade off RAM for less frequent GC's)
 ;; 2024-02-02: Also from https://github.com/jwiegley/dot-emacs/blob/master/init.org
-(setq gc-cons-percentage 0.6
-      gc-cons-threshold (* 128 1024 1024))
+(setq garbage-collection-messages t
+      gc-cons-percentage 0.6
+      gc-cons-threshold (* 8 1024 1024 1024)) ; 8GB
 
 ;; Prefer loading newest compiled .el file and turn off
-(setq load-prefer-newer noninteractive
-      inhibit-startup-message   t)
+(setq load-prefer-newer       noninteractive
+      inhibit-startup-message t
+      frame-resize-pixelwise  t
+      )
 
-(menu-bar-mode   -1)
-(scroll-bar-mode -1)
-(tool-bar-mode   -1)
-(tooltip-mode    -1)
+(blink-cursor-mode 0)
+(menu-bar-mode    -1)
+(scroll-bar-mode  -1)
+(set-fringe-mode  10)
+(tool-bar-mode    -1)
+(tooltip-mode     -1)
 
 ;; Setting background color here to prevent color flash as the theme gets activated.
 (setq default-frame-alist '((background-color        . "#1d2021") ;; Same background color as our theme
