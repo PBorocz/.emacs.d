a95de1d9fb4d781a0cdbb2095d157d28ba9d334e
Peter Borocz <680893+PBorocz@users.noreply.github.com>
2026-02-17 17:39:28 -0800

    Move to light color theme (easier on the older eyes).

--- early-init.el
+++ early-init.el
@@ -1,13 +1,16 @@
 ;; Startup speed, annoyance suppression
 ;; (ht: https://git.sr.ht/~ashton314/emacs-bedrock/tree/main/item/early-init.el)
-(setq gc-cons-threshold                        10000000)
-(setq byte-compile-warnings                    '(not obsolete))
-(setq warning-suppress-log-types               '((comp) (bytecomp)))
-(setq native-comp-async-report-warnings-errors 'silent)
-
-(setq load-prefer-newer       t ;; Ensure Emacs loads the most recent byte-compiled files.
-      inhibit-startup-message t ;; Turn off startup messages.
-      frame-resize-pixelwise  t)
+;; (ht: https://github.com/jamescherti/compile-angel.el)
+(setq byte-compile-warnings                    '(not obsolete)
+      frame-resize-pixelwise                   t
+      gc-cons-threshold                        10000000
+      inhibit-startup-message                  t ;; Turn off startup messages.
+      initial-major-mode                       'fundamental-mode ;; Make the initial buffer load faster by setting its mode to fundamental-mode
+      load-prefer-newer                        t ;; Ensure Emacs loads the most recent byte-compiled files.
+      native-comp-async-report-warnings-errors 'silent
+      native-comp-jit-compilation              t
+      warning-suppress-log-types               '((comp) (bytecomp))
+      )
 
 ;; Turn off window embellishments
 ;; (doing here as they don't seem to work when loaded from config.org)
@@ -16,6 +19,3 @@
 (set-fringe-mode 10)
 (tool-bar-mode   -1)
 (tooltip-mode    -1)
-
-;; Make the initial buffer load faster by setting its mode to fundamental-mode
-(setq initial-major-mode 'fundamental-mode)
