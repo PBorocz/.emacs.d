ede66cea44b1f61de98febe5120a05c3080599ec
Peter Borocz <680893+PBorocz@users.noreply.github.com>
2026-02-21 09:28:25 -0800

    Finally got to the bottom of org-mode block snippets.

--- snippets/org-mode/.yas-compiled-snippets.el
+++ snippets/org-mode/.yas-compiled-snippets.el
@@ -2,20 +2,19 @@
 ;;; Snippet definitions:
 ;;;
 (yas-define-snippets 'org-mode
-		     '(("blockp"
-			"#+begin_src python\n  $0\n#+end_src\n"
-			"#+beginp_ <lang> ...#+end_" nil nil nil
-			"/Users/peter/.emacs.d/snippets/org-mode/blockp"
+		     '(("srcp" "#+begin_src python\n  $0\n#+end_src"
+			"srcp" nil nil nil
+			"/Users/peter/.emacs.d/snippets/org-mode/srcp"
 			nil nil)
-		       ("blocke"
-			"#+begin_src emacs-lisp\n  $0\n#+end_src\n"
-			"#+begin <lang> ...#+end_" nil nil nil
-			"/Users/peter/.emacs.d/snippets/org-mode/blocke"
+		       ("srce"
+			"#+begin_src emacs-lisp\n  $0\n#+end_src"
+			"srce" nil nil nil
+			"/Users/peter/.emacs.d/snippets/org-mode/srce"
 			nil nil)
-		       ("block_" "#+begin_src $0\n  $1\n#+end_src\n"
-			"#+begin <lang> ...#+end_" nil nil nil
-			"/Users/peter/.emacs.d/snippets/org-mode/block_"
+		       ("src" "#+begin_src $1\n  $0\n#+end_src" "src"
+			nil nil nil
+			"/Users/peter/.emacs.d/snippets/org-mode/src"
 			nil nil)))
 
 
-;;; Do not edit! File generated at Thu Feb  5 11:34:40 2026
+;;; Do not edit! File generated at Sat Feb 21 09:28:06 2026
--- snippets/python-base-mode/.yas-compiled-snippets.el
+++ snippets/python-base-mode/.yas-compiled-snippets.el
@@ -4,25 +4,24 @@
 (yas-define-snippets 'python-base-mode
 		     '(("super"
 			"super(`(replace-regexp-in-string \"\\\\([.]\\\\)[^.]+$\" \", self).\" (python-info-current-defun) nil nil 1)`($1)\n$0\n"
-			"super" nil ("object oriented") nil
+			"super" nil nil nil
 			"/Users/peter/.emacs.d/snippets/python-base-mode/super"
 			nil nil)
 		       ("print_" "print(f\"{$0}\")\n" "print" nil nil
 			nil
 			"/Users/peter/.emacs.d/snippets/python-base-mode/print_"
 			nil nil)
-		       ("pp" "from pprint import pprint as pp\n"
-			"import_pprint" nil ("debug") nil
-			"/Users/peter/.emacs.d/snippets/python-base-mode/pp"
+		       ("pp_" "from pprint import pprint as pp\n"
+			"import_pprint" nil nil nil
+			"/Users/peter/.emacs.d/snippets/python-base-mode/pp_"
 			nil nil)
 		       ("main_"
-			"def main():\n    $0\n\nif __name__ == \"__main__\":\n   main()\n"
-			"main_" nil nil nil
+			"def main():\n    pass\n\nif __name__ == \"__main__\":\n   main()\n"
+			"main" nil nil nil
 			"/Users/peter/.emacs.d/snippets/python-base-mode/main_"
 			nil nil)
 		       ("for" "for ${var} in ${collection}:\n    $0\n"
-			"for ... in ... : ..." nil
-			("control structure") nil
+			"for ... in ... : ..." nil nil nil
 			"/Users/peter/.emacs.d/snippets/python-base-mode/for"
 			nil nil)
 		       ("ds_" "\"\"\"$0.\"\"\"\n" "docstring" nil nil
@@ -31,7 +30,7 @@
 			nil nil)
 		       ("dataclass"
 			"from dataclasses import dataclass\n\n@dataclass\nclass ${1:class}:\n    $0\n"
-			"dataclass" nil ("object oriented") nil
+			"dataclass" nil nil nil
 			"/Users/peter/.emacs.d/snippets/python-base-mode/dataclass"
 			nil nil)
 		       ("bp" "breakpoint()\n" "breakpoint" nil nil nil
@@ -42,4 +41,4 @@
 			nil nil)))
 
 
-;;; Do not edit! File generated at Thu Feb  5 11:34:40 2026
+;;; Do not edit! File generated at Sat Feb 21 09:28:06 2026
